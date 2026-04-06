106ffb64ff50d41c00376f9cda0c57047f4f7ac0
SDK-China <90819364+SDK-China@users.noreply.github.com>
2026-04-06 14:01:20 +0800

    新增數據、更新QA01數據、更新查詢

--- FactoryEntryReport.js
+++ FactoryEntryReport.js
@@ -459,7 +459,9 @@ const LOC_CONFIGS = {
                 "NDExNTI0MjAwNTEyMTA3NjU2", //杨瑞 
                 // "MDg5NjQ3MzI=", //赖彦翔 
                 // "MDYyNDg5MDE=", //马可为
-                // "SDAzODMzNTcy", //冼延浩
+                // "WjkwOTQwMSg3KQ==", //冼延浩 (新)
+                "NDQxNDgxMTk4ODAzMTYwODky", //张远彬 (新)
+                "MDcyMjg1Nzc=", //朱会民 (新)
                 // "NTMyNDY5ODc0" //Denis Gerassimenko
                 "NDIyMzI2MTk5NTA0Mjg2NDEx" //竇桂陽
             ],
--- visitorApprovalQuery.js
+++ visitorApprovalQuery.js
@@ -43,7 +43,9 @@ const CONFIGS = {
             "NDExNTI0MjAwNTEyMTA3NjU2",
             "MDg5NjQ3MzI=",
             "MDYyNDg5MDE=",
-            "SDAzODMzNTcy",
+            "WjkwOTQwMSg3KQ==", //冼延浩 (新)
+            "NDQxNDgxMTk4ODAzMTYwODky", //张远彬 (新)
+            "MDcyMjg1Nzc=", //朱会民 (新)
             "NTMyNDY5ODc0",
             "NDIyMzI2MTk5NTA0Mjg2NDEx" //竇桂陽
         ],
